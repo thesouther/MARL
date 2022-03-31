2cecd3f28c8f0a0e8ed0bac5ab8bfe7c791321d3
cicoa <1183864793@qq.com>
2022-03-31 10:19:10 +0800

    123

--- QMIX/policy.py
+++ QMIX/policy.py
@@ -1,8 +1,9 @@
 import torch
-import os 
+import os
 from NN import DRQN, QMIXNET
 # import sys
-# sys.setrecursionlimit(100000) #例如这里设置为十万 
+# sys.setrecursionlimit(100000) #例如这里设置为十万
+
 
 class QMIX:
     def __init__(self, conf):
@@ -19,7 +20,7 @@ class QMIX:
         # DRQN 的参数
         if self.conf.last_action:
             input_shape += self.n_actions
-        if self.conf.reuse_network: 
+        if self.conf.reuse_network:
             input_shape += self.n_agents
 
         # NET
@@ -72,7 +73,7 @@ class QMIX:
 
         s, s_, u, r, avail_u, avail_u_, terminated = batch['s'], batch['s_'], batch['u'], batch['r'], \
                                                     batch['avail_u'], batch['avail_u_'], batch['terminated']
-        mask = 1 - batch['padded'].float() # 把填充经验的TD-error置0，防止影响学习
+        mask = 1 - batch['padded'].float()  # 把填充经验的TD-error置0，防止影响学习
 
         # 得到每个agent对应的Q值，维度为(episode个数, max_episode_len， n_agents， n_actions)
         q_evals, q_targets = self.get_q_values(batch, max_episode_len)
@@ -89,16 +90,16 @@ class QMIX:
         q_targets[avail_u_ == 0.0] = -9999999
         q_targets = q_targets.max(dim=3)[0]
         # print("q_evals2 shape: ", q_evals.size()) # [batch_size, max_episode_len, n_agents]
-        
+
         q_total_eval = self.eval_qmix_net(q_evals, s)
         q_total_target = self.target_qmix_net(q_targets, s_)
 
-        targets = r + self.conf.gamma * q_total_target *    (1-terminated)
+        targets = r + self.conf.gamma * q_total_target * (1 - terminated)
 
         td_error = (q_total_eval - targets.detach())
         mask_td_error = mask * td_error
 
-        loss = (mask_td_error ** 2).sum() / mask.sum()
+        loss = (mask_td_error**2).sum() / mask.sum()
         self.optimizer.zero_grad()
         loss.backward()
         torch.nn.utils.clip_grad_norm_(self.eval_parameters, self.conf.grad_norm_clip)
@@ -107,46 +108,45 @@ class QMIX:
         if train_step > 0 and train_step % self.conf.update_target_params == 0:
             self.target_drqn_net.load_state_dict(self.eval_drqn_net.state_dict())
             self.target_qmix_net.load_state_dict(self.target_qmix_net.state_dict())
-        
 
     def get_q_values(self, batch, max_episode_len):
         episode_num = batch['o'].shape[0]
         q_evals, q_targets = [], []
         for transition_idx in range(max_episode_len):
-            inputs, inputs_ = self._get_inputs(batch, transition_idx) # 给obs加last_action、agent_id
+            inputs, inputs_ = self._get_inputs(batch, transition_idx)  # 给obs加last_action、agent_id
             inputs = inputs.to(self.device)  # [batch_size*n_agents, obs_shape+n_agents+n_actions]
             inputs_ = inputs_.to(self.device)
-            
+
             self.eval_hidden = self.eval_hidden.to(self.device)
             self.target_hidden = self.target_hidden.to(self.device)
-            q_eval, self.eval_hidden = self.eval_drqn_net(inputs, self.eval_hidden) # (n_agents, n_actions)
+            q_eval, self.eval_hidden = self.eval_drqn_net(inputs, self.eval_hidden)  # (n_agents, n_actions)
             q_target, self.target_hidden = self.target_drqn_net(inputs_, self.target_hidden)
 
-            q_eval = q_eval.view(episode_num, self.n_agents, -1) #(batch_size, n_agents, n_actions)
+            q_eval = q_eval.view(episode_num, self.n_agents, -1)  #(batch_size, n_agents, n_actions)
             q_target = q_target.view(episode_num, self.n_agents, -1)
             q_evals.append(q_eval)
             q_targets.append(q_target)
-        
+
         # 得的q_eval和q_target是一个列表，列表里装着max_episode_len个数组，数组的的维度是(episode个数, n_agents，n_actions)
         # 把该列表转化成(batch_size, max_episode_len， n_agents，n_actions)的数组
         q_evals = torch.stack(q_evals, dim=1)
         q_targets = torch.stack(q_targets, dim=1)
         return q_evals, q_targets
 
-
     def _get_inputs(self, batch, transition_idx):
-        o, o_, u_onehot = batch['o'][:, transition_idx], batch['o_'][:, transition_idx], batch['u_onehot'][:] # u_onehot取全部，要用上一条
-        episode_num = o.shape[0] # batch_size
+        o, o_, u_onehot = batch['o'][:, transition_idx], batch['o_'][:, transition_idx], batch[
+            'u_onehot'][:]  # u_onehot取全部，要用上一条
+        episode_num = o.shape[0]  # batch_size
         inputs, inputs_ = [], []
         inputs.append(o)
         inputs_.append(o_)
-        
+
         # 给obs添加上一个动作、agent编号
         if self.conf.last_action:
             if transition_idx == 0:
                 inputs.append(torch.zeros_like(u_onehot[:, transition_idx]))
             else:
-                inputs.append(u_onehot[:, transition_idx-1])
+                inputs.append(u_onehot[:, transition_idx - 1])
             inputs_.append(u_onehot[:, transition_idx])
 
         if self.conf.reuse_network:
@@ -161,8 +161,8 @@ class QMIX:
         # 把batch_size、n_agents个agent的obs拼起来，
         # 因为这里所有agent共享一个神经网络，每条数据中带上了自己的编号，所以还是自己的数据
         # (batch_size, n_agents, n_actions) -> (batch_size*n_agents, n_actions)
-        inputs = torch.cat([x.reshape(episode_num*self.n_agents, -1) for x in inputs], dim=1)
-        inputs_ = torch.cat([x.reshape(episode_num*self.n_agents, -1) for x in inputs_], dim=1)
+        inputs = torch.cat([x.reshape(episode_num * self.n_agents, -1) for x in inputs], dim=1)
+        inputs_ = torch.cat([x.reshape(episode_num * self.n_agents, -1) for x in inputs_], dim=1)
 
         return inputs, inputs_
 
@@ -175,6 +175,5 @@ class QMIX:
         if not os.path.exists(self.model_dir):
             os.makedirs(self.model_dir)
         print("save model: {} epoch.".format(num))
-        torch.save(self.eval_drqn_net.state_dict(), self.model_dir+'/'+num+'_drqn_net_params.pkl')
-        torch.save(self.eval_qmix_net.state_dict(), self.model_dir+'/'+num+'_qmix_net_params.pkl')
-
+        torch.save(self.eval_drqn_net.state_dict(), self.model_dir + '/' + num + '_drqn_net_params.pkl')
+        torch.save(self.eval_qmix_net.state_dict(), self.model_dir + '/' + num + '_qmix_net_params.pkl')
